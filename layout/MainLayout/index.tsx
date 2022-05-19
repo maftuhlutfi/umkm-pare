8f23952c833bb2c5fb954c057f0162d120d62f10
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-19 09:56:14 +0700

    main layout path

--- layout/MainLayout/index.tsx
+++ layout/MainLayout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Footer from '../components/shared/Footer'
-import Header from '../components/shared/Header'
+import Footer from '../../components/shared/Footer'
+import Header from '../../components/shared/Header'
 
 type Props = {
     children: React.ReactNode
