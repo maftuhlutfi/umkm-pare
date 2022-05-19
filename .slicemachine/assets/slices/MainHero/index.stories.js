21f01959468b54352880063e8d583c179b48567e
maftuhlutfi <playmakernyomlo@gmail.com>
2022-05-19 09:05:40 +0700

    content modelling with slicemachine

---
+++ .slicemachine/assets/slices/MainHero/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MainHero';
+
+export default {
+  title: 'slices/MainHero'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"main_hero","items":[],"primary":{"title":[{"type":"heading1","text":"Reintermediate sexy e-business","spans":[]}],"description":[{"type":"paragraph","text":"Commodo aliquip magna dolore eu dolor anim eu officia pariatur. Magna amet amet sunt sunt dolor magna elit eiusmod dolor eiusmod qui ut voluptate. Reprehenderit pariatur ex aliqua esse consectetur adipisicing laborum cupidatat ea cillum et pariatur exercitation.","spans":[]}],"hero-image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?w=900&h=500&fit=crop"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
