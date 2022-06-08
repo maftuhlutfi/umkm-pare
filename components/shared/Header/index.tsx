6d8f95963df67aad67619cd461bbe4afd42d3dba
maftuhlutfi <playmakernyomlo@gmail.com>
2022-06-08 18:41:59 +0700

    add link to nav logo

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -13,7 +13,11 @@ const Header = (props: Props) => {
     return (
         <div className="fixed top-0 z-50 w-full py-8 bg-white">
             <div className="relative mx-auto flex w-[90%] max-w-[1444px] items-center justify-between lg:w-full lg:px-10 xl:px-20">
-                <Image src="/logo.svg" width={120} height={32} />
+                <Link href="/">
+                    <a>
+                        <Image src="/logo.svg" width={120} height={32} />
+                    </a>
+                </Link>
                 <div className="flex items-center gap-12">
                     {menuList.map(({ title, href }, index) => (
                         <Link key={index} href={href}>
