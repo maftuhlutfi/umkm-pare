ff3da89aa70a232952103194c78de84c0008517f
maftuhlutfi <playmakernyomlo@gmail.com>
2022-06-08 18:40:48 +0700

    add link to product item

--- components/Products/ProductItem/index.tsx
+++ components/Products/ProductItem/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { ProductType } from '../../../ts/types/Product'
 import currencyFormatter from '../../../utils/currencyFormatter'
@@ -15,30 +16,35 @@ const ProductItem = ({
         umkm: { uid: umkmUid, name: umkmName },
         price,
         id,
+        uid,
     },
 }: Props) => {
     return (
-        <div className="flex flex-col border-2 border-gray-900">
-            <Image
-                src={pictures[0].url}
-                width={512}
-                height={512}
-                objectFit="cover"
-                alt={pictures[0].alt}
-            />
-            <div className="p-4 bg-gray-900">
-                <div className="mb-3">
-                    <h4 className="text-lg font-semibold text-white">{name}</h4>
-                    <p className="text-sm text-gray-200">{umkmName}</p>
+        <Link href={`/${umkmUid}/${uid}`}>
+            <a className="flex flex-col border-2 border-gray-900">
+                <Image
+                    src={pictures[0].url}
+                    width={512}
+                    height={512}
+                    objectFit="cover"
+                    alt={pictures[0].alt}
+                />
+                <div className="p-4 bg-gray-900">
+                    <div className="mb-3">
+                        <h4 className="text-lg font-semibold text-white">
+                            {name}
+                        </h4>
+                        <p className="text-sm text-gray-200">{umkmName}</p>
+                    </div>
+                    <p className="mb-4 text-xl font-bold text-white">
+                        {currencyFormatter(price)}
+                    </p>
+                    <Button type="dark" full className="py-3 text-base">
+                        + Keranjang
+                    </Button>
                 </div>
-                <p className="mb-4 text-xl font-bold text-white">
-                    {currencyFormatter(price)}
-                </p>
-                <Button type="dark" full className="py-3 text-base">
-                    + Keranjang
-                </Button>
-            </div>
-        </div>
+            </a>
+        </Link>
     )
 }
 
