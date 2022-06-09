72b31e284660254a4bd66f54f1fb28b4bd557219
maftuhlutfi <playmakernyomlo@gmail.com>
2022-06-09 08:23:31 +0700

    add next progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,25 @@ import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { linkResolver, repositoryName } from '../prismicio'
 import Link from 'next/link'
+import NextProgress from 'next-progress'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <PrismicProvider
-            linkResolver={linkResolver}
-            internalLinkComponent={({ href, children, ...props }) => (
-                <Link href={href}>
-                    <a {...props}>{children}</a>
-                </Link>
-            )}
-        >
-            <PrismicPreview repositoryName={repositoryName}>
-                <Component {...pageProps} />
-            </PrismicPreview>
-        </PrismicProvider>
+        <>
+            <NextProgress options={{ showSpinner: false }} color="#000000" />
+            <PrismicProvider
+                linkResolver={linkResolver}
+                internalLinkComponent={({ href, children, ...props }) => (
+                    <Link href={href}>
+                        <a {...props}>{children}</a>
+                    </Link>
+                )}
+            >
+                <PrismicPreview repositoryName={repositoryName}>
+                    <Component {...pageProps} />
+                </PrismicPreview>
+            </PrismicProvider>
+        </>
     )
 }
 
