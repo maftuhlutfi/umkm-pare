a8383066e1f6f47aa29e39a8b745764744f9b58d
maftuhlutfi <playmakernyomlo@gmail.com>
2022-06-16 08:46:58 +0700

    responsive header

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Buy } from 'react-iconly'
 import useCartStore from '../../../store/useCartStore'
 import menuList from './menuList'
@@ -12,6 +12,11 @@ type Props = {}
 const Header = (props: Props) => {
     const { pathname } = useRouter()
     const { cart } = useCartStore()
+    const [showMenu, setShowMenu] = useState(false)
+
+    useEffect(() => {
+        setShowMenu(false)
+    }, [pathname])
 
     const cartItemsTotal = cartItemsCount(cart)
 
@@ -19,11 +24,15 @@ const Header = (props: Props) => {
         <div className="fixed top-0 z-50 w-full py-8 bg-white">
             <div className="relative mx-auto flex w-[90%] max-w-[1444px] items-center justify-between lg:w-full lg:px-10 xl:px-20">
                 <Link href="/">
-                    <a>
+                    <a className="z-10 shrink-0">
                         <Image src="/logo.svg" width={120} height={32} />
                     </a>
                 </Link>
-                <div className="flex items-center gap-12">
+                <div
+                    className={`fixed inset-0 ${
+                        showMenu ? '' : 'translate-x-full lg:translate-x-0'
+                    } flex transform flex-col items-center gap-12 bg-white pt-32 transition-all duration-200 lg:static lg:flex-row lg:pt-0`}
+                >
                     {menuList.map(({ title, href }, index) => (
                         <Link key={index} href={href}>
                             <a
@@ -38,16 +47,38 @@ const Header = (props: Props) => {
                         </Link>
                     ))}
                 </div>
-                <Link href="/cart">
-                    <a className="relative">
-                        <div className="absolute -right-1 -top-0.5 h-4 w-4 rounded-full bg-red-600">
-                            <p className="absolute text-xs font-semibold text-white transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                                {cartItemsTotal}
-                            </p>
-                        </div>
-                        <Buy set="bold" size={32} />
-                    </a>
-                </Link>
+                <div className="flex items-center">
+                    <Link href="/cart">
+                        <a className="relative">
+                            <div className="absolute -right-1 -top-0.5 h-4 w-4 rounded-full bg-red-600">
+                                <p className="absolute text-xs font-semibold text-white transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                    {cartItemsTotal}
+                                </p>
+                            </div>
+                            <Buy set="bold" size={32} />
+                        </a>
+                    </Link>
+                    <div
+                        className="z-10 w-6 ml-8 bg-red-500 cursor-pointer lg:hidden"
+                        onClick={() => setShowMenu((prev) => !prev)}
+                    >
+                        <span
+                            className={`absolute block h-0.5 w-6 transform rounded-full bg-gray-900 transition duration-500 ease-in-out ${
+                                showMenu ? 'rotate-45' : '-translate-y-2'
+                            }`}
+                        />
+                        <span
+                            className={`absolute block h-0.5 w-6 transform rounded-full bg-gray-900 transition duration-500 ease-in-out ${
+                                showMenu && '-translate-x-6 opacity-0'
+                            }`}
+                        />
+                        <span
+                            className={`absolute block h-0.5 w-6 transform rounded-full bg-gray-900 transition duration-500 ease-in-out ${
+                                showMenu ? '-rotate-45' : 'translate-y-2'
+                            }`}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
